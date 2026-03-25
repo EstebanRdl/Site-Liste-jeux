92bdcdedaa97dc5489d961f13c32d904f3818463
esteban.ridel <esteban.ridel@etudiant.univ-rennes.fr>
2026-03-25 18:05:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Charger le fichier JSON
+fetch("games.json")
+  .then(response => response.json())
+  .then(data => {
+    afficherJeux(data);
+
+    // Recherche
+    document.getElementById("search").addEventListener("input", (e) => {
+      const valeur = e.target.value.toLowerCase();
+
+      const filtres = data.filter(jeu =>
+        jeu.nom.toLowerCase().includes(valeur)
+      );
+
+      afficherJeux(filtres);
+    });
+  });
+
+function afficherJeux(jeux) {
+  const container = document.getElementById("game-list");
+  container.innerHTML = "";
+
+  jeux.forEach(jeu => {
+    const card = document.createElement("div");
+    card.classList.add("game-card");
+
+    card.innerHTML = `
+      <img src="images/${jeu.image}.jpg" alt="${jeu.nom}">
+      <p>${jeu.nom}</p>
+    `;
+
+    container.appendChild(card);
+  });
+}
